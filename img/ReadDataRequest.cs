14a90a90eebd3b1cb181fc4c8996282100c3ccd3
tra7dfz <weiting926@qq.com>
2022-05-05 09:07:43 +0800

    Upload by PicGo

---
+++ img/ReadDataRequest.cs
@@ -0,0 +1,18 @@
+﻿using BrightJade;
+
+namespace FashionStar.Servo.Uart.Protocol
+{
+    [PacketSerializable]
+    public class ReadDataRequest : RequestHeader
+    {
+        [PacketField]
+        public byte ID;
+
+        [PacketField]
+        public byte DataID;
+
+        public ReadDataRequest() : base(PacketConst.ReadData)
+        {
+        }
+    }
+}
